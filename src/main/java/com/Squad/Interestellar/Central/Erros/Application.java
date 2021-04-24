674bf954e37aea3a045daecf6e9376d939a67f60
PedroMarqDev <PedroMarqDev@gmail.com>
2021-04-24 16:32:32 -0300

    Configuração inicial

---
+++ src/main/java/com/Squad/Interestellar/Central/Erros/Application.java
@@ -0,0 +1,13 @@
+package com.Squad.Interestellar.Central.Erros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Squad/Interestellar/Central/Erros/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Squad.Interestellar.Central.Erros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
