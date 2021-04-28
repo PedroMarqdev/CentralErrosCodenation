23e9f823827b43f4646dd7d20152fd975c69aea9
Vitor Rodrigues <vitor_rc@outlook.com>
2021-04-28 12:28:00 -0300

    MVC user

---
+++ src/main/java/com/Squad/Interestellar/Central/Erros/service/impl/UserService.java
@@ -0,0 +1,34 @@
+package com.Squad.Interestellar.Central.Erros.service.impl;
+
+import com.Squad.Interestellar.Central.Erros.entity.User;
+import com.Squad.Interestellar.Central.Erros.repository.UserRepository;
+import com.Squad.Interestellar.Central.Erros.service.interfaces.UserServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements UserServiceInterface {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Optional<User> findByLogin(String login) {
+        return userRepository.findByLogin(login);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User save(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
