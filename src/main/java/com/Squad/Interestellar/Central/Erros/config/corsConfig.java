c56c3134ec6c535e1dee1d1b9264900169e234a7
Pedro Paulo Marques da Costa <pedromarqdev@gmail.com>
2021-05-03 16:10:15 -0300

    Update corsConfig.java

--- src/main/java/com/Squad/Interestellar/Central/Erros/config/corsConfig.java
+++ src/main/java/com/Squad/Interestellar/Central/Erros/config/corsConfig.java
@@ -4,6 +4,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");// <- assim permite de qualquer origem, troque "/**" pelo seu dominio por exemplo "http://meudominio.com"
+        registry.addMapping("/**");// <- Obrigado Nando Luiz pelo código no stackoverflow!
     }
 }
