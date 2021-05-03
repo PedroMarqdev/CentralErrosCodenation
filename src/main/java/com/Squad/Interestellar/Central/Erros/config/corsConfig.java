4357ae4e217d5fa7cca65e71370178b15b6764c1
PedroMarqDev <PedroMarqDev@gmail.com>
2021-05-03 16:16:32 -0300

    Imports corsConfig

--- src/main/java/com/Squad/Interestellar/Central/Erros/config/corsConfig.java
+++ src/main/java/com/Squad/Interestellar/Central/Erros/config/corsConfig.java
@@ -1,3 +1,10 @@
+package com.Squad.Interestellar.Central.Erros.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 @EnableWebMvc
 public class corsConfig implements WebMvcConfigurer {
